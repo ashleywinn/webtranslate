6c691364a0a660a534d1d38f89d3193268c7a266
Ashley Winn <ashley.winn@gmail.com>
2014-07-08 15:46:31 +0800

    added putonghua app and got an empty webpage displayed

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_enter_chinese_text_and_retrieve_it_later(self):
+        # Steve goes to check out a new translation website
+        self.browser.get('http://localhost:8000')
+
+        # Its called Translation Builder
+        assert 'Translation Builder' in self.browser.title
+
+        # There is a text box for entering a chinese phrase
+
+        # After the phrase is entered definitions are shown for the characters and words
+        # and a new text box appears for the user to enter their translation
+
+        # The input is then re-printed, Chinese above the English with 
+        # check boxes next to each character and word
+
+        # The user then marks the Chinese characters and the corresponding English and hits submit
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
 
