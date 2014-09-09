b58ecaa3c65855b4567329463b9ef3dec6387b0d
Ashley Winn <ashley.winn@gmail.com>
2014-09-09 12:42:07 +0800

    changed the root path from 'putonghua' to 'reader'

--- webtranslate/urls.py
+++ webtranslate/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$',       'putonghua.views.home_page', name='home'),
-    url(r'^putonghua/', include('putonghua.urls')),
+    url(r'^reader/', include('putonghua.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
